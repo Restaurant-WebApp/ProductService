a1eb217cf9dfd194223dc240fe8278443012efc3
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-03-21 23:12:49 +0100

    Product Model and Controller created

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using ProductAPI.Model;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+               new Product{ ProductID = 0, ProductName = "Chicken Curry", ProductDescription = "Delicious chicken curry", ProductCategory = "Mains", ProductImage = "image1" },
+               new Product{ ProductID = 1, ProductName = "Momos", ProductDescription = "Delicious momo", ProductCategory = "Starter", ProductImage = "image2" },
+               new Product{ ProductID = 2, ProductName = "Biryani", ProductDescription = "Delicious Biryani", ProductCategory = "Mains", ProductImage = "image3" }
+        };
+
+        [HttpGet]
+        [Route ("/products")]
+        [ProducesResponseType (typeof (Product), 200)]
+        [ProducesResponseType (400)]
+        public IActionResult GetProducts()
+        {
+            if (products == null) {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route ("/id")]
+        public ActionResult<Product> GetProductById(int id)
+        {
+            var product = products.Find(product => product.ProductID == id);
+            if (product != null) { return Ok(product); }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            if (product == null) { return BadRequest(); }
+            products.Add(product);
+            return Ok(products);
+        }
+    }
+}
---
+++ ProductAPI/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductAPI.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductImage { get; set; }
+
+        public Product() { }    
+
+        public Product(int productID, string productName, string productDescription, string productCategory, string productImage)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            ProductDescription = productDescription;
+            ProductCategory = productCategory;
+            ProductImage = productImage;
+        }
+    }
+}
