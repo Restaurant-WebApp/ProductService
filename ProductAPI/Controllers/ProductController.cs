3e078cdc22a3f8d8bb804825c1a0411eba7a00c7
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-03-24 14:43:59 +0100

    Added new api calls (GetById, Put, Post and Delete)

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper.Configuration.Conventions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using ProductAPI.Model;
 using ProductAPI.Repository;
+using System.Formats.Asn1;
 
 namespace ProductAPI.Controllers
 {
@@ -36,6 +38,73 @@ namespace ProductAPI.Controllers
             return _response;
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> GetProductById(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepo.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto newProduct = await _productRepo.CreateUpdateProduct(productDto);
+                _response.Result = newProduct;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto newProduct = await _productRepo.CreateUpdateProduct(productDto);
+                _response.Result = newProduct;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+
+        [HttpDelete]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepo.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
         /*[HttpGet]
         [Route ("/id")]
         public ActionResult<Product> GetProductById(int id)
